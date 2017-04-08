44bd835587b858b879e97330680e63069b5d0d1c
keisatou <dummy>
2017-04-08 23:46:58 +0900

    make Components dynamic and cached

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
     <div class="row">
       <div class="col-xs-12">
         <br>
-        <button class="btn btn-primary">Load Blue Template</button>
-        <button class="btn btn-success">Load Green Template</button>
-        <button class="btn btn-danger">Load Red Template</button>
+        <button class="btn btn-primary" @click="selectedComponent = 'app-blue'">Load Blue Template</button>
+        <button class="btn btn-success" @click="selectedComponent = 'app-green'">Load Green Template</button>
+        <button class="btn btn-danger" @click="selectedComponent = 'app-red'">Load Red Template</button>
         <hr>
-        <app-blue></app-blue>
-        <app-green></app-green>
-        <app-red></app-red>
+        {{ selectedComponent }}
+        <keep-alive>
+          <component :is="selectedComponent"></component>
+        </keep-alive>
       </div>
     </div>
   </div>
@@ -21,6 +22,11 @@ import Green from './components/Green.vue';
 import Red from './components/Red.vue';
 
 export default {
+  data() {
+    return {
+      selectedComponent: 'app-blue',
+    };
+  },
   components: {
     appBlue: Blue,
     appGreen: Green,
